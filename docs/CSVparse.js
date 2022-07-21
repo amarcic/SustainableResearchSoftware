00dc4544c14f9159545faf79f2c3ec30798c6ca2
amarcic <aleksander.marcic@gmail.com>
2022-07-21 10:52:07 +0200

    Create CSVparse.js

---
+++ docs/CSVparse.js
@@ -0,0 +1,11 @@
+//uses PapaParse; wrapps the parser in a promise
+
+export async function CSVparse(CSVfile) {
+  return new Promise( (resolve, reject) => {
+    Papa.parse( CSVfile, {
+      download: true,
+      complete: results => resolve(results),
+      error: error => reject(error)
+    } )
+  });
+}
