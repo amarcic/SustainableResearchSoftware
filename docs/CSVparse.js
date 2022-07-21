02dc1bd721ab81822ce44ea5afae1fe75bb01658
amarcic <aleksander.marcic@gmail.com>
2022-07-21 15:40:02 +0200

    Update CSVparse.js
    fix empty row bug

--- docs/CSVparse.js
+++ docs/CSVparse.js
@@ -4,6 +4,7 @@ export async function CSVparse(CSVfile) {
   return new Promise( (resolve, reject) => {
     Papa.parse( CSVfile, {
       download: true,
+      skipEmptyLines: true,
       complete: results => resolve(results),
       error: error => reject(error)
     } )
